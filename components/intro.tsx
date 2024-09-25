62709ff1b35ac7b68cdcdb0b83955797f0cbeac1
Cole House <30875198+Cole-House@users.noreply.github.com>
2024-09-25 13:41:21 -1000

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,6 +4,10 @@ import Image from 'next/image'
 import React from 'react'
 import myImage from '@/public/myPortrait.jpg'
 import { motion } from "framer-motion";
+import Link from 'next/link';
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
   return (
@@ -28,7 +32,7 @@ export default function Intro() {
                     // priority will get this to load first 
                     priority={true}
                     className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
-                />
+                  />
                     <motion.span 
                         className="absolute bottom-0 right-0 text-4xl"
                         initial={{ opacity: 0, scale: 0 }}
@@ -57,6 +61,44 @@ export default function Intro() {
             building <span className="italic">web-apps</span>. that improve{" "}
             <span className="underline">people's lives</span>.
         </motion.h1>
+
+        <motion.div 
+            className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+                delay: 0.1,
+              }}
+        >
+            <Link 
+                href="#contact"
+                className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+            >
+                Contact Me Here {" "}<BsArrowRight className='opacity-70 group-hover:translate-x-1 transition'/>
+            </Link>
+            <a
+                className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+                href="/Cole.pdf"
+                download
+             >
+                Download CV{" "}
+                <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+            </a>
+            <a
+                className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+                href="https://linkedin.com"
+                target="_blank"
+            >
+                <BsLinkedin />
+            </a>
+            <a
+                className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+                href="https://github.com"
+                target="_blank"
+            >
+                <FaGithubSquare />
+            </a>
+        </motion.div>
 
     </section>
   )
